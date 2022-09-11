03e48ff24c7d802f5bf6d1c4cbcda198ee3917f3
Bassam AL SAEDY <basaidi@gmail.com>
2022-09-11 09:03:28 +0200

    filal commit

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
 from turtle import fillcolor
 import qrcode
+
+
+def generate_qrcode(text):
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4
+    )
+
+    qr.add_data(text)
+    qr.make(fit=True)
+    img = qr.make_image(fill_color="black", back_color="white")
+    img.save("qrimg.png")
+
+
+# generate_qrcode("https://bassams.github.io/")
+url = input("Enter url to Generate QRCode: ")
+generate_qrcode(url)
